103b2b56b57f83b473c35fdc45acd2b57dfec4b3
hsuchihting <hsuchihting@gmail.com>
2025-07-21 22:20:18 +0800

    fix: 修正 Header 組件的結構，確保內容正確顯示

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,9 @@
 <template>
-	<div class="w-full">
+	<div class="w-full ">
+		<div class="lg:w-[1200px] md:flex md:item-end md:justify-between md:mx-auto w-full">
 			<h1 class="text-3xl font-bold text-white tracking-wide ">當日蔬菜與海鮮資訊</h1>
 			<p class=" text-white tracking-wide">更新時間： {{ today }}</p>   
+		</div>
 	</div>
 </template>
 
