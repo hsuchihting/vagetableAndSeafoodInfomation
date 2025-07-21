860fecb2c78387774b07ede28a9894830313d8c1
hsuchihting <hsuchihting@gmail.com>
2025-07-21 14:34:28 +0800

    feat: 更新 Vite 配置，新增伺服器設定與路徑別名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    port: 3000,
+    open: true // 自動開啟瀏覽器
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  define: {
+    __VUE_PROD_DEVTOOLS__: false,   // 生產環境中禁用開發工具
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false  // 禁用水合不匹配詳細信息
+  }
+});
