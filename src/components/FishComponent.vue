29826919fa2358de9889b253c593fd9550d4091a
hsuchihting <hsuchihting@gmail.com>
2025-07-21 22:31:05 +0800

    fix: 更新 FishComponent 和 VegetableComponent 組件，修正顯示邏輯並轉換為 script setup 語法

--- src/components/FishComponent.vue
+++ src/components/FishComponent.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-    <p>this is fish component</p>
+    <p v-if="fishClosed">今日休市</p>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { toRefs, computed } from 'vue';
+const props = defineProps({
+  fisheryProducts: {
+    type: Array,
+    default: () => []
+  }
+});
+const { fisheryProducts } = toRefs(props);
 
-}
+const fishClosed = computed(()=> {
+  return fisheryProducts.value?.find(item => item.SeafoodProdName === '休市');
+});
+
+console.log(fishClosed.value);
 </script>
 
 <style>
--- src/components/VegetableComponent.vue
+++ src/components/VegetableComponent.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <p>this is vegetable component</p>
+    <p v-if="vegetableClosed">今日休市</p>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import { toRefs, computed } from 'vue';
+const props = defineProps({
+  agriProducts: {
+    type: Array,
+    default: () => []
+  }
+});
+const { agriProducts } = toRefs(props);
+const vegetableClosed = computed(()=> {
+  return agriProducts.value.find(item => item.CropName === '休市');
+});
 </script>
-
-<style>
-
-</style>
