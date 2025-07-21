b9b45ae74abec441d8f2797b34012ef4c96bd5b6
hsuchihting <hsuchihting@gmail.com>
2025-07-21 22:46:11 +0800

    fix: 更新 url.js 中的註解，增強 API 說明的清晰度

--- src/api/url.js
+++ src/api/url.js
@@ -2,12 +2,20 @@ import { get } from './methods';
 
 const baseUrl = 'https://data.moa.gov.tw/api/v1/';
 
-// 漁產品交易
+/**
+ * 農漁產品交易 API
+ * @name FisheryProductsTransType - 漁產品交易類型
+ * @name AgriProductsTransType - 農產品交易類型
+ */
 const fisheryProductsTransType = `${baseUrl}/FisheryProductsTransType`;
-// 農產品交易
 const AgriProductsTransType = `${baseUrl}/AgriProductsTransType`;
 
-// 包裝 API 呼叫方法
+/**
+ * @name getFisheryProducts - 獲取漁產品交易數據
+ * @param {Object} params - 查詢參數
+ * @name getAgriProducts - 獲取農產品交易數據
+ * @param {Object} params - 查詢參數
+ */
 export const getFisheryProducts = (params = {}) => {
   return get(fisheryProductsTransType, params);
 };
